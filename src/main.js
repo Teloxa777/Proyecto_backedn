167d930e949e605128261391c8e6129e6269141c
TheAgentP4 <diegomr090@gmail.com>
2023-10-15 18:02:34 -0600

    add:➕ Agregada BD y middlewares

--- src/main.js
+++ src/main.js
@@ -2,6 +2,27 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import '@/assets/css/main.css'
+import express from "express"
+import 'dotenv/config'
+import user from "./routes/user.route.js"
+import {errorHandler} from "./middlewares/error.middleware.js"
+
+const app = express()
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+app.get('/', (req, res) => {
+    res.send('Hello World')
+})
+
+app.use('/user',user)
+app.use('/user/find',user)
+app.use(errorHandler)
+
+
+app.listen(process.env.PORT, () => {
+    console.log("Server running on port " + process.env.PORT)
+})
 
 
 createApp(App).mount('#app')
